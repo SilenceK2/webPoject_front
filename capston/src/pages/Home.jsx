6f7a4d6d9dc363a1a4ce247f336eaab2d1785b97
JaeSeung0529 <vww2021@naver.com>
2024-04-11 00:51:47 +0900

    Home 화면 구성

--- capston/src/pages/Home.jsx
+++ capston/src/pages/Home.jsx
@@ -1,28 +1,117 @@
-import Footer from "../style/stylecomponents/Layout/Footer";
+
 import Header from "../style/stylecomponents/Layout/Header";
 import styled from "styled-components";
+import { BottomSection, Topsection } from "../style/stylecomponents/LoginStyle";
+
+
+
 const PageContainer = styled.div`
-  display: flex;
+
   flex-direction: column;
   min-height: 100vh;
+
+
 `;
-const Content = styled.div`
-  flex: 1;
-  width: 100%;
-  height: 100%;
+
+const WeatherContent = styled.div`
+  padding: 30px;
+  margin: 30px;
+  flex-direction: column;
+  background-color: #41C9E2;
+  border-radius : 10px;
+  width: 380px;
+  height: 210px;  
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
 `;
+
+const WeatherTop = styled.div`
+display: flex;
+flex: 1;
+
+`;
+
+const WeatherBottom = styled.div`
+display:flex;
+flex: 1;
+  
+  
+`;
+
+const WeatherIcon = styled.div`
+display: flex;
+flex: 0.35;
+  width: 100%;
+
+  flex-direction: column;
+`;
+
+const WeatherInfo = styled.div`
+
+flex: 0.65;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const WeatherSetTimeBar = styled.div`
+flex: 1;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const GoToContent = styled.div`
+padding: 30px;
+margin: 30px;
+flex-direction: column;
+background-color: #41C9E2;
+border-radius : 10px;
+width: 380px;
+height: 210px; 
+display: flex;
+`
+
+const ToDoContent = styled.div`
+padding: 30px;
+margin: 30px;
+flex-direction: column;
+background-color: #41C9E2;
+border-radius : 10px;
+width: 380px;
+height: 210px; 
+display: flex;
+`
+const BottomFooter = styled.div`
+  background-color: 41C9E2#;
+  flex: 1;
+  display: flex;
+  width: 500px;
+  height: 150px;
+  background-color: #;
+`
 
 const Home = () => {
   return (
     <>
       <PageContainer>
         <Header>header</Header>
-        <Content></Content>
-        <Footer></Footer>
+        <BottomSection>     
+          <WeatherContent>
+            <WeatherTop>
+              <WeatherIcon>Weather Icon</WeatherIcon>
+              <WeatherInfo>Weather Info</WeatherInfo>
+            </WeatherTop>
+            <WeatherBottom>
+              <WeatherSetTimeBar>Weather Set Time Bar</WeatherSetTimeBar>
+            </WeatherBottom>
+          </WeatherContent>
+          <GoToContent></GoToContent>
+          <ToDoContent></ToDoContent>
+        </BottomSection>
+
+        <BottomFooter>
+
+        </BottomFooter>
       </PageContainer>
     </>
   );
