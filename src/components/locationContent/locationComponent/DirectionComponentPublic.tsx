37fce401989b32c9ca2ac903fcdf4acdbf19325b
JaeSeung0529 <vww2021@naver.com>
2024-06-17 19:12:10 +0900

    서버 파일 생성 DirectionComponentPublic 경로 제한 추가

--- src/components/locationContent/locationComponent/DirectionComponentPublic.tsx
+++ src/components/locationContent/locationComponent/DirectionComponentPublic.tsx
@@ -13,7 +13,7 @@ import {
   Circle // 추가된 부분
 } from "../styles";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCar, faPerson, faTrain,faBus } from '@fortawesome/free-solid-svg-icons'; // 추가된 부분
+import { faCar, faPerson, faTrain, faBus } from '@fortawesome/free-solid-svg-icons'; // 추가된 부분
 
 interface Place {
   title: string;
@@ -178,11 +178,13 @@ const DirectionComponentPublic: React.FC<DirectionComponentPublicProps> = ({ sta
     }
   };
 
+  const MIN_WIDTH = 20; // 최소 폭 설정
+
   return (
     <>
       {route && (
         <DirectionComponentPublicSection>
-          {route.path.map((path, index) => (
+          {route.path.slice(0, 5).map((path, index) => ( // 경로를 5개로 제한
             <div key={index}>
               <DirectionComponentPublicBox>
                 <DirectionComponentPublicBoxHeader>
@@ -195,53 +197,58 @@ const DirectionComponentPublic: React.FC<DirectionComponentPublicProps> = ({ sta
                   <p style={{ fontSize: "20px", fontWeight: "bold" }}>&nbsp;⊸ {path.info.payment}원 </p>
                 </DirectionComponentPublicBoxBox>
                 <DirectionComponentBar>
-                  {path.subPath.map((subPath, subIndex) => (
-                    <div key={subIndex} style={{ position: 'relative', display: 'inline-block', width: `${(subPath.sectionTime / path.info.totalTime) * 100}%`, textAlign: 'center' }}>
-                      {subIndex !== path.subPath.length - 1 && (
-                        <Circle
+                  {path.subPath.filter(subPath => subPath.distance > 0 && subPath.sectionTime > 0).map((subPath, subIndex) => {
+                    const subPathWidth = Math.max((subPath.sectionTime / path.info.totalTime) * 100, MIN_WIDTH);
+
+                    return (
+                      <div key={subIndex} style={{ position: 'relative', display: 'inline-block', width: `${subPathWidth}%`, textAlign: 'center' }}>
+                        {subIndex !== path.subPath.length - 1 && (
+                          <Circle
+                            style={{
+                              position: 'absolute',
+                              top: '-3px',
+                              left: '0',
+                              transform: 'translateX(-50%)',
+                              backgroundColor:
+                                subPath.trafficType === 1 ? "#87b4fb" : subPath.trafficType === 2 ? "#1ed71e" : "lightgray",
+                              width: '25px',
+                              height: '25px',
+                              borderRadius: '50%',
+                              zIndex: 1,
+                              display: 'flex',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              color: 'white', // 아이콘 색상
+                            }}
+                          >
+                            {getIcon(subPath.trafficType)}
+                          </Circle>
+                        )}
+                        <DirectionComponentPublicBar
                           style={{
-                            position: 'absolute',
-                            top: '-5px',
-                            left: '0',
-                            transform: 'translateX(-50%)',
+                            borderRadius: subIndex === path.subPath.length - 1 ? "0px 10px 10px 0px" : "0px",
                             backgroundColor:
                               subPath.trafficType === 1 ? "#87b4fb" : subPath.trafficType === 2 ? "#1ed71e" : "lightgray",
-                            width: '30px',
-                            height: '30px',
-                            borderRadius: '50%',
+                            width: '100%',
+                            height: '15px',
                             zIndex: 1,
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            color: 'white', // 아이콘 색상
                           }}
-                        >
-                          {getIcon(subPath.trafficType)}
-                        </Circle>
-                      )}
-                      <DirectionComponentPublicBar
-                        style={{
-                          borderRadius: subIndex === path.subPath.length - 1 ? "0px 10px 10px 0px" : "0px",
-                          backgroundColor:
-                            subPath.trafficType === 1 ? "#87b4fb" : subPath.trafficType === 2 ? "#1ed71e" : "lightgray",
-                          width: '100%',
-                          zIndex: 1,
-                        }}
-                      ></DirectionComponentPublicBar>
-                      <div style={{ position: 'absolute', top: '20px', width: '100%', fontSize: '14px', fontWeight: 'bold' }}>
-                        {subPath.trafficType !== 3 && subPath.lane && subPath.lane[0] && (
-                          <>
-                            {subPath.lane[0].busNo || subPath.lane[0].name}
-                          </>
+                        ></DirectionComponentPublicBar>
+                        <div style={{ position: 'absolute', top: '20px', width: '100%', fontSize: '11px', fontWeight: 'bold' }}>
+                          {subPath.trafficType !== 3 && subPath.lane && subPath.lane[0] && (
+                            <>
+                              {subPath.lane[0].busNo || subPath.lane[0].name}
+                            </>
+                          )}
+                        </div>
+                        {subPath.distance !== 0 && (
+                          <div style={{ position: 'absolute', top: '0px', width: '100%', fontSize: '9px' }}>
+                            {Math.round(subPath.sectionTime)}분
+                          </div>
                         )}
                       </div>
-                      {subPath.distance !== 0 && (
-                        <div style={{ position: 'absolute', top: '0px', width: '100%', fontSize: '12px' }}>
-                          {Math.round(subPath.sectionTime)}분
-                        </div>
-                      )}
-                    </div>
-                  ))}
+                    );
+                  })}
                 </DirectionComponentBar>
               </DirectionComponentPublicBox>
             </div>
